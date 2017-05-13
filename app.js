4bae0cb3d4175e09cf39e64ca67024a2386ce4e0
babkamen <babkamen@gmail.com>
2017-05-14 02:04:21 +0300

    Finish

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 var assert = require("assert");
 var mongoose = require('mongoose');
+var http = require("https");
 
 var uri = process.env.MONGOLAB_URI || 'mongodb://localhost/urls';
+var apiKey = process.env.API_KEY ;
+var cx = process.env.CX ;
 // Use bluebird
 var options = {
 	promiseLibrary: require('bluebird')
@@ -31,21 +34,74 @@ app.get('/', function(req, res) {
 
 
 app.get('/api/imagesearch/:term', function(req, res) {
+
+	var term = req.params.term;
+	var offset = req.query.offset || 0;
+	console.log("Term=", term);
+
 	var termLogEntry = TermLogEntry({
-		term:req.params.term
+		term: term
 	});
 	termLogEntry.save();
-	res.send({
-		"text": "url"
-	});
+
+	var a = json => {
+		var resp = [];
+		for (var a of json.items) {
+
+			resp.push({
+				url: a.link,
+				snippet: a.title + "|" + a.displayLink,
+				thumbnail: a.image.thumbnailLink,
+				context: a.image.contextLink
+
+			});
+		}
+
+		res.send(resp);
+	};
+
+
+	searchImages(term, offset, a);
 });
+
+function searchImages(term, offset, callback) {
+	var path = "/customsearch/v1?key="+apiKey+"&cx="+cx+"&" +
+		"searchType=image&fileType=jpg&imgSize=xlarge&safe=high&&alt=json&q=" + encodeURIComponent(term) + "&start=" + offset;
+	console.log("Path=", path);
+	var options = {
+		"method": "GET",
+		"hostname": "www.googleapis.com",
+		"port": null,
+		"path": path,
+
+	};
+
+	var clreq = http.request(options, function(clres) {
+		var chunks = [];
+
+		clres.on("data", function(chunk) {
+			chunks.push(chunk);
+		});
+
+		clres.on("end", function() {
+			var body = Buffer.concat(chunks);
+			var json = JSON.parse(body.toString())
+			callback(json);
+		});
+	});
+
+	clreq.end();
+}
+
 
 
 app.get('/api/latest/imagesearch', function(req, res) {
-
+	TermLogEntry.find({},{_id:0,__v:0}).sort({when:-1}).limit(10).then(function(f){
+		res.send(f);	
+	})
 });
 
-app.set('port', (process.env.PORT || 4001));
+app.set('port', (process.env.PORT || 4003));
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
